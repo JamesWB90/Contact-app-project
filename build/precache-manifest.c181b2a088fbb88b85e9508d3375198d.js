2b03a6943a0028d19aa3f15ff987c219200a68e0
JamesWB90 <baileyjames48@gmail.com>
2022-09-29 00:11:38 +0100

    update

---
+++ build/precache-manifest.c181b2a088fbb88b85e9508d3375198d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439cc071410b193aca5196e89f31dbec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "05a4e37d5a1cddddfe2a",
+    "url": "/static/js/main.1dd6b3b7.chunk.js"
+  },
+  {
+    "revision": "44105ad7c8f6d07310b3",
+    "url": "/static/js/runtime-main.a19405b8.js"
+  }
+]);
